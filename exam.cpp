7716ad1c372597099946e2564a1a3e7e75f32dee
waht <qwerty@111.com>
2023-09-30 23:50:25 +0800

    remove has_begin_end, judge it by hands instead

--- exam.cpp
+++ exam.cpp
@@ -3,19 +3,14 @@
 using namespace std;
 
 int main() {
-    print(1, 2, "hello", Params{.sep=",,", .end="\t"});
-    print(vector<int>{1, 2, 3, 45}, 1,Params{.sep=","});
+    print(1, 2, "hello");
     print();
 
     map<int,string> x; x[1]="0"; x[15]="xx"; x[33]="64";
     stack<int> s; s.push(1); s.push(15); s.push(18);
     std::queue<int> q;    q.push(1);    q.push(2);    q.push(3);
-    print(x,"hello",s,' ',q,vector<int>{},Params{.sep=" | "});
 
     std::ofstream file("test.txt");
-    bitset<8> b(string("01110000"));
-    print("hello",b,13,Params{.out=&file});
 
-    vector<vector<int>> vs{{1,2,3},{4,5,6},{7,8,9}};
-    print(vs);
+    print(x,"hello",s,' ',q,vector<vector<int>>{{}},Params{.sep=" | ",.out=&file});
 }
--- print.h
+++ print.h
@@ -8,6 +8,11 @@
 #include <bitset>
 #include <queue>
 #include <unordered_map>
+#include <type_traits>
+#include <list>
+#include <forward_list>
+#include <set>
+#include <unordered_set>
 
 struct Params {
     std::string sep = " ";
@@ -15,6 +20,87 @@ struct Params {
     std::ostream *out = &std::cout;
 };
 namespace pstyle {
+
+    template<typename Inst, template<typename...> typename Tmpl>
+    struct is_instantiation_of : std::false_type {
+    };
+
+    template<template<typename...> typename Tmpl, typename... Args>
+    struct is_instantiation_of<Tmpl<Args...>, Tmpl> : std::true_type {
+    };
+
+    template<typename T, typename U, typename... Rest>
+    struct is_one_of : std::conditional_t<
+            std::is_same_v<T, U>, std::true_type, is_one_of<T, Rest...>> {
+    };
+
+    template<typename T, typename U>
+    struct is_one_of<T, U> : std::conditional_t<
+            std::is_same_v<T, U>, std::true_type, std::false_type> {
+    };
+
+    template<typename Inst, template<typename...> typename Tmpl,
+            template<typename...> typename... Rest>
+    struct is_one_instantiation_of : std::conditional_t<
+            is_instantiation_of<Inst, Tmpl>::value, std::true_type, is_one_instantiation_of<Inst, Rest...>> {
+    };
+
+    template<typename Inst, template<typename...> typename Tmpl>
+    struct is_one_instantiation_of<Inst, Tmpl> : std::conditional_t<
+            is_instantiation_of<Inst, Tmpl>::value, std::true_type, std::false_type> {
+    };
+
+    template<typename Inst, template<typename...> typename Tmpl,
+            template<typename...> typename... Rest>
+    const bool is_one_instantiation_of_v = is_one_instantiation_of<Inst, Tmpl, Rest...>::value;
+
+    template<typename T>
+    typename std::enable_if_t<is_one_of<std::decay_t<T>, int, long, long long, unsigned,
+            unsigned long, unsigned long long,
+            float, double, long double, std::string, char*, char>::value>
+    _print(const T &x, const Params &params = {}) {
+        *(params.out) << x;
+    }
+
+    template<typename T>
+    std::enable_if_t<std::is_same_v<std::decay_t<T>,bool>>
+    _print(const T& v, const Params &params = {}){
+        *(params.out)<<(v?"true":"false");
+    }
+
+    template<typename Ts>
+    typename std::enable_if_t<is_one_instantiation_of_v<std::decay_t<Ts>,
+            std::vector, std::deque, std::list, std::forward_list,
+            std::set, std::multiset, std::unordered_set, std::unordered_multiset>>
+    _print(const Ts &args, const Params &params = {}) {
+        bool first = true;
+        *(params.out) << '[';
+        for (const auto &i: args) {
+            if (!first) *(params.out) << ',';
+            first = false;
+            _print(i,params);
+        }
+        *(params.out) << ']';
+    }
+
+    template<typename T>
+    std::enable_if_t<
+            is_one_instantiation_of_v<
+                    std::decay_t<T>, std::map, std::unordered_map, std::multimap, std::unordered_multimap>>
+    _print(const T &m, const Params &params = {}) {
+        *(params.out) << '{';
+        bool first = true;
+        for (const auto &it: m) {
+            if (!first) *(params.out) << ',';
+            first = false;
+            _print(it.first,params);
+            *(params.out) << ':';
+            _print(it.second,params);
+        }
+        *(params.out) << '}';
+    }
+
+
     template<class T>
     void _print(const std::stack<T> &x, const Params &params = {}) {
         std::stack<T> s1(x), s2;
@@ -23,11 +109,11 @@ namespace pstyle {
             s1.pop();
         }
         *(params.out) << '[';
-        bool first= true;
+        bool first = true;
         while (!s2.empty()) {
             if (!first) *(params.out) << ',';
             first = false;
-            *(params.out) << s2.top() ;
+            _print(s2.top(),params);
             s2.pop();
         }
         *(params.out) << ']';
@@ -37,38 +123,14 @@ namespace pstyle {
     void _print(const std::queue<T> &x, const Params &params = {}) {
         auto q2 = x;
         *(params.out) << '[';
-        bool first= true;
+        bool first = true;
         while (!q2.empty()) {
             if (!first) *(params.out) << ',';
             first = false;
-            *(params.out) << q2.front();
+            _print(q2.front(),params);
             q2.pop();
         }
         *(params.out) << ']';
-    }
-
-    template<class Ta, class Tb>
-    void _print(const std::map<Ta, Tb> &m, const Params &params = {}) {
-        *(params.out) << '{';
-        bool first= true;
-        for (const auto &it: m) {
-            if (!first) *(params.out) << ',';
-            first = false;
-            *(params.out) << it.first << ':' << it.second;
-        }
-        *(params.out) << '}';
-    }
-
-    template<class Ta, class Tb>
-    void _print(const std::unordered_map<Ta, Tb> &m, const Params &params = {}) {
-        *(params.out) << '{';
-        bool first= true;
-        for (const auto &it: m) {
-            if (!first) *(params.out) << ',';
-            first = false;
-            *(params.out) << it.first << ':' << it.second;
-        }
-        *(params.out) << '}';
     }
 
     template<size_t s>
@@ -86,40 +148,17 @@ namespace pstyle {
     template<std::size_t I = 0, typename... Ts>
     void _print(const std::tuple<Ts...> &tuple, const Params &params = {}) {
         if constexpr (I < sizeof...(Ts)) {
-            *(params.out) << std::get<I>(tuple);
+            _print(std::get<I>(tuple),params);
             if constexpr (I != sizeof...(Ts) - 1) *(params.out) << ',';
-            _print<I + 1>(tuple);
+            _print<I + 1>(tuple,params);
         }
     }
 
     template<typename Ta, typename Tb>
     void _print(const std::pair<Ta, Tb> &x, const Params &params = {}) {
-        *(params.out) << x.first << ',' << x.second;
-    }
-
-    template<typename T, typename=void>
-    struct has_begin_end : std::false_type {
-    };
-    template<typename T>
-    struct has_begin_end<T, std::void_t<decltype(std::declval<T>().begin()), decltype(std::declval<T>().end())>>
-            : std::true_type {
-    };
-
-    template<typename Ts>
-    void _print(const Ts &args, const Params &params = {}) {
-        if constexpr (std::is_same_v<Ts, std::string> || !has_begin_end<Ts>::value) {
-            *(params.out) << args;
-        } else {
-            bool first = true;
-            *(params.out) << '[';
-            for (const auto &i: args) {
-                if (!first) *(params.out) << ',';
-                first = false;
-                _print(i);
-
-            }
-            *(params.out) << ']';
-        }
+        _print(x.first,params);
+        _print(',',params);
+        _print(x.second,params);
     }
 }
 
@@ -128,13 +167,13 @@ void sep(const Params &p) {
 }
 
 template<typename T>
-typename std::enable_if<std::is_same_v<T,Params>>::type
+typename std::enable_if<std::is_same_v<T, Params>>::type
 print(const T &p) {
     *(p.out) << p.end;
 }
 
 template<typename... Ts>
-typename std::enable_if<sizeof...(Ts)==0>::type
+typename std::enable_if<sizeof...(Ts) == 0>::type
 print() {
     std::cout << "\n";
 }
